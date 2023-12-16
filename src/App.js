259fcab5caba2df814339dcad7bcd79b9b93900d
Milly <liuhongmein@gmail.com>
2023-12-16 18:41:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => {
+      return wait(1000).then(() => [...POSTS]);
+    },
+    staleTime: 1000,
+    // queryFn: () => Promise.reject("Error message"),
+  });
+
+  const { isLoading, data, error } = postsQuery;
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) =>
+      wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title })),
+    onSuccess: () => {
+      queryClient.invalidateQueries("posts");
+    },
+  });
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tanstack Query</h1>
+      {data.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+      <button
+        disabled={newPostMutation.isPending}
+        onClick={() => newPostMutation.mutate("new")}
+      >
+        Add New
+      </button>
     </div>
   );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
